7d8d5712147d19e5163899b3d284cd7705b073c8
Liao Chuxin <maxinechuxin@gmail.com>
2016-10-23 08:05:34 -0700

    Finish other ui

--- app/src/main/java/homefulfriends/twood/ChildMainActivity.java
+++ app/src/main/java/homefulfriends/twood/ChildMainActivity.java
@@ -2,6 +2,7 @@ package homefulfriends.twood;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class ChildMainActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class ChildMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_child_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Home");
     }
 }
---
+++ app/src/main/java/homefulfriends/twood/SavingsTransferActivity.java
@@ -0,0 +1,13 @@
+package homefulfriends.twood;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SavingsTransferActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_savings_transfer);
+    }
+}
